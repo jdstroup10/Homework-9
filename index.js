82b4548eefc7f2cd766c6a6c14028b787f68d4c8
Jason Strouphauer <jdstroup10@gmail.com>
2020-04-09 23:30:19 -0500

    committing first changes to js files

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
-const questions = [
+//const questions = [
 
-];
+//];
 
-function writeToFile(fileName, data) {
-}
+//function writeToFile(fileName, data) {
+//}
 
-function init() {
+//function init() {
 
-}
+//}
 
-init();
+//init();
+
+var inquirer = require("inquirer");
+const axios = require("axios");
+var fs = require('fs');
+
+inquirer.prompt( {
+    type: "input",
+    name: "username",
+    message: "What is your Github username?"
+  })
+
+  .then(function({ username }) {
+    const GithubURL = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(GithubURL).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("readme.md", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+       // console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+    inquirer.prompt( {
+        type: "input",
+        name: "username",
+        message: "What Repo do you want to look at?"
+      })
+      
+  });
+
+  
+  
+
+  
+
+  
+
+
+  //var filename = "Readme.md"
+  //var md = `${data.name}`
+  
+ 
+
+  //fs.writeFile(filename, md, function(err) {
+
+    //if (err) {
+    //  return console.log(err);
+   // }
+
+    //console.log("Success!");
+
+  //});
+//});
