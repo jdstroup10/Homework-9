7b248a468449feb1cdef2904b41db8a3023a4a50
Jason Strouphauer <jdstroup10@gmail.com>
2020-04-10 12:02:15 -0500

    Committing base working js file. Still need to add user picture and badges

--- ReadMeOutput.md
+++ ReadMeOutput.md
@@ -1,1 +1,34 @@
-Jason
+
+    
+  
+      #TITLE
+
+      BIG CAT FINDER
+  
+      ##Description:
+      PROJECT THAT HELPS U FIND BIG CATS
+  
+      ##Collaborators
+      Team lead: jdstroup10
+      Team members: ME + SOME OTHERS
+
+      ##Table of Contents
+      NO TABLE OF CONTENTS
+
+      ##Installation
+      This project was installed using the following method: GITHUB
+
+      ##Testing
+      This project was testing using the following methods: MANUALLY
+
+      ##Common Q&A's
+      Questions: NOPE
+
+      Answers: NO ANSWERS
+
+      ##Licensing
+      NONE
+
+
+      Auto-generated readme file courtesy of jdstroup10
+    
--- index.js
+++ index.js
@@ -15,13 +15,114 @@ var inquirer = require("inquirer");
 const axios = require("axios");
 var fs = require('fs');
 
-inquirer.prompt( {
+
+inquirer.prompt( [{
     type: "input",
     name: "username",
     message: "What is your Github username?"
-  })
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Please enter a title for your new project"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Add a description for your new project"
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Who is contributing to this project?"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What is the license on this project?"
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "How would you like to install this project?"
+  },
+  {
+    type: "input",
+    name: "contents",
+    message: "Enter your table of contents here:"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How are you testing your project?"
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Are there any common questions about this project?"
+  },
+  {
+    type: "input",
+    name: "answers",
+    message: "Write your answer to the common questions here."
+  },
+  
+  
 
-  .then(function({ username }) {
+  
+  
+]).then(function(data) {
+
+    var filename = "readMeOutput.md"
+    var md = `
+    
+  
+      #TITLE
+
+      ${data.title}
+  
+      ##Description:
+      ${data.description}
+  
+      ##Collaborators
+      Team lead: ${data.username}
+      Team members: ${data.contribute}
+
+      ##Table of Contents
+      ${data.contents}
+
+      ##Installation
+      This project was installed using the following method: ${data.install}
+
+      ##Testing
+      This project was testing using the following methods: ${data.test}
+
+      ##Common Q&A's
+      Questions: ${data.questions}
+
+      Answers: ${data.answers}
+
+      ##Licensing
+      ${data.license}
+
+
+      Auto-generated readme file courtesy of jdstroup10
+    `
+  
+    fs.writeFile(filename, md, function(err) {
+  
+      if (err) {
+        return console.log(err);
+      }
+  
+      console.log("Success!");
+  
+    });
+  });
+
+  
+
+  /*.then(function({ username }) {
     const GithubURL = `https://api.github.com/users/${username}/repos?per_page=100`;
 
     axios.get(GithubURL).then(function(res) {
@@ -43,9 +144,9 @@ inquirer.prompt( {
         type: "input",
         name: "username",
         message: "What Repo do you want to look at?"
-      })
+      }) 
       
-  });
+  }); */
 
   
   
